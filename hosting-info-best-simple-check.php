fd1a67cd5e006a31f443708de234957cf97355da
seo101 <seo101@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-18 13:20:01 +0000

    FirstVersion
    git-svn-id: https://plugins.svn.wordpress.org/hosting-info-best-simple-check/trunk@1070395 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hosting-info-best-simple-check.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ Plugin Name: Hosting Info - Best Simple Check
+ Plugin URI:
+ Description: Displays information on your WordPress hosting
+ Version: 1.0
+ Author: <a href="http://www.seo101.net">Seo101</a>
+ */
+
+function wordpress_hosting_info_page() {
+?>
+<h4>Wordpress Hosting Information</h4>
+<BR><BR>
+<?php
+		global $wpdb;
+		$php_version = PHP_VERSION;
+		$mysql_version = $wpdb->db_version();
+		echo 'MYSQL VERSION = ' . $mysql_version . '<BR><BR>';
+		echo 'PHP VERSION = ' . $php_version . '<BR><BR>';
+		echo 'PHP INFO:' . '<BR>';
+ob_start();
+phpinfo();
+preg_match ('%<style type="text/css">(.*?)</style>.*?(<body>.*</body>)%s', ob_get_clean(), $matches);
+echo "<div class='classforinfodispl'><style type='text/css'>\n",
+    join( "\n",
+        array_map(
+            create_function(
+                '$i',
+                'return ".classforinfodispl " . preg_replace( "/,/", ",.classforinfodispl ", $i );'
+                ),
+            preg_split( '/\n/', $matches[1] )
+            )
+        ),
+    "</style>\n",
+    $matches[2],
+    "\n</div>\n";
+}
+
+
+function wordpress_hosting_info_menu() {
+	if (is_admin()) {
+		add_options_page('WP Hosting Info', 'WP Hosting Info', 'administrator','wordpress_hosting_info', 'wordpress_hosting_info_page');
+	}
+}
+
+// Admin menu items
+add_action('admin_menu', 'wordpress_hosting_info_menu');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Hosting Info - Best Simple Check ===
+Contributors: seo101
+Donate link: 
+Tags: hosting, php version, php info
+Requires at least: 3.1
+Tested up to: 4.1
+Stable tag: 1.00
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays information on your WordPress hosting like mysql version and php version information.
+
+== Description ==
+
+Displays information on your WordPress hosting like mysql version and php version information.
+
+
+== Installation ==
+
+1. Upload the zip file to the `/wp-content/plugins/` directory and unzip
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Use the plugin from the settings menu
+
+== Frequently asked questions ==
+
+None yet
+
+== Screenshots ==
+
+1. 
+2. 
+3. 
+4. 
+
+== Changelog ==
+1.00 Release
+
+
+== Upgrade notice ==
+
+
+
+== Arbitrary section 1 ==
